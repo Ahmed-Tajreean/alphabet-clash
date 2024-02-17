914b3402e6ff69305ed5f33402f9c0afd336e93b
Ahmed-Tajreean <ahmedtajreean@gmail.com>
2024-02-17 10:43:00 +0600

    score updated

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,45 +1,64 @@
-function hideElementId(elementId) {
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function showElementId(elementId) {
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
-function setBackgroundColorId(elementId) {
+function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
 
-function removeBackgroundColorId(elementId) {
+function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
 
-function getARandomAlphabet() {
-    const alphabetString = 'abcdefghijklmnopqrstuvxyz';
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
 
-    // get a random index from 0 to 25
-    const randomNumber = Math.random() * 25;
+    const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
 
     const alphabet = alphabets[index];
-    // console.log(index, alphabet);
     return alphabet;
-    // console.log(alphabet[Math.round(Math.random() * alphabet.length)]);
-
 }
 
 // function getARandomAlphabet() {
-//     const alphabetString = 'abcdefghijklmnopqrstuvxyz';
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
 //     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
 
-//     const randomNumber = Math.random() * 25
+//     // get a random index between 0 -25
+//     const randomNumber = Math.random() * 25;
 //     const index = Math.round(randomNumber);
-
+    
 //     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
 //     return alphabet;
 // }
